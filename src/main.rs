40632946b36c81a9200883fd67c7c6ac0ffe1de6
Alex Rose <alexander.rose@pcc.edu>
2021-06-04 11:30:58 -0700

    Added map into main

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,12 @@ mod map;
 use map::*;
 mod rect;
 pub use rect::Rect;
+mod player;
+use player::*;
 
-struct State {
+pub struct State {
     ecs: World
 }
-
-#[derive(Component, Debug)]
-pub struct Monster {}
 
 pub struct MonsterAI {}
 
@@ -32,30 +31,6 @@ impl<'a> System<'a> for MonsterAI {
     }
 }
 
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79 , max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // Player movement
-    match ctx.key {
-        None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-
-            _ => {}
-        },
-    }
-}
 
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
@@ -63,6 +38,8 @@ impl GameState for State {
 
         player_input(self, ctx);
         self.run_systems();
+
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -110,6 +87,10 @@ fn main() -> rltk::BError {
         .with(Monster{})
         .build();
 
+        let map : Map = Map::new_map_walls();
+        gs.ecs.insert(map);
+
+
     //spawn 10 monsters
     let mut rng = rltk::RandomNumberGenerator::new();
     for i in 1..=10 {
@@ -124,6 +105,7 @@ fn main() -> rltk::BError {
             3 => { glyph = rltk::to_cp437('*') }
             _ => { glyph = rltk::to_cp437('^') }
         }
+        
 
         gs.ecs.create_entity()
             .with(Position{ x, y })
